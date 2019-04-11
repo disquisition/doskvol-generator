58080fd3ffa5fd919b263dcc72cdf330264d439c
Spenser Isdahl <sisdahl@conversantmedia.com>
2019-04-11 15:21:03 -0500

    Add reactstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Col, Row } from 'reactstrap';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>Work in progress...</Col>
+      </Row>
+    </Container>
+  );
 }
-
-export default App;
