7fa8777300ae4af76644938d630c98f3c5063d94
Spenser Isdahl <sisdahl@conversantmedia.com>
2020-06-09 11:00:59 -0500

    Demon generator feedback

--- src/components/Demon.tsx
+++ src/components/Demon.tsx
@@ -32,13 +32,40 @@ function DemonLook() {
   const appears = useFlattenedRule(`#[${pronounsRule}]demon-appears#`);
   const aspect = useFlattenedRule(`#[${pronounsRule}]demon-aspect.a#`);
   const aspectIncludesWith = aspect.includes(' with ');
-  const feature1 = useFlattenedRule(
+
+  const feature1Rule = useExpandedRule(
     `#[${pronounsRule}]demon-feature-physical-1#`
   );
-  const feature2 = useFlattenedRule(
+  const feature1Prefix = useFlattenedRule(
+    `#[${pronounsRule}][${feature1Rule}]feature-prefix#`
+  );
+  const feature1Highlight = useFlattenedRule(
+    `#[${pronounsRule}][${feature1Rule}]feature-highlight#`
+  );
+  const feature1Postfix = useFlattenedRule(
+    `#[${pronounsRule}][${feature1Rule}]feature-postfix#`
+  );
+
+  const feature2Rule = useExpandedRule(
     `#[${pronounsRule}]demon-feature-physical-2#`
   );
-  const feature = useFlattenedRule(`#[${pronounsRule}]demon-feature-physical#`);
+  const feature2Prefix = useFlattenedRule(
+    `#[${pronounsRule}][${feature2Rule}]feature-prefix#`
+  );
+  const feature2Highlight = useFlattenedRule(
+    `#[${pronounsRule}][${feature2Rule}]feature-highlight#`
+  );
+  const feature2Postfix = useFlattenedRule(
+    `#[${pronounsRule}][${feature2Rule}]feature-postfix#`
+  );
+
+  const [featureRandom] = useRandom();
+  const featurePrefix = featureRandom >= 0.5 ? feature1Prefix : feature2Prefix;
+  const featureHighlight =
+    featureRandom >= 0.5 ? feature1Highlight : feature2Highlight;
+  const featurePostfix =
+    featureRandom >= 0.5 ? feature1Postfix : feature2Postfix;
+
   const aura = useFlattenedRule(`#[${pronounsRule}]demon-feature-aura#`);
 
   const [random] = useRandom();
@@ -49,8 +76,18 @@ function DemonLook() {
         The demon <Text className={styles.demonName} source={name} />{' '}
         <Text source={appears} />—<Text source={aspect} emphasis="primary" />{' '}
         {aspectIncludesWith ? 'who has' : 'with'}{' '}
-        <Text source={feature1} emphasis="secondary" /> and{' '}
-        <Text source={feature2} emphasis="secondary" />.
+        <Text source={feature1Prefix} />{' '}
+        <Text source={feature1Highlight} emphasis="secondary" />{' '}
+        <Text source={feature1Postfix} /> and <Text source={feature2Prefix} />{' '}
+        <Text source={feature2Highlight} emphasis="secondary" />
+        {feature2Postfix ? (
+          <Fragment>
+            {' '}
+            <Text source={feature2Postfix} />.
+          </Fragment>
+        ) : (
+          '.'
+        )}
       </Fragment>
     );
   }
@@ -59,9 +96,20 @@ function DemonLook() {
     <Fragment>
       The demon <Text className={styles.demonName} source={name} />{' '}
       <Text source={appears} />—<Text source={aspect} emphasis="primary" />{' '}
-      {aspectIncludesWith ? 'and' : 'with'}{' '}
-      <Text source={feature} emphasis="secondary" />, whose presence causes{' '}
-      <Text source={aura} emphasis="secondary" />.
+      {aspectIncludesWith ? 'and' : 'with'} <Text source={featurePrefix} />{' '}
+      <Text source={featureHighlight} emphasis="secondary" />
+      {featurePostfix ? (
+        <Fragment>
+          {' '}
+          <Text source={featurePostfix} />,
+        </Fragment>
+      ) : (
+        ','
+      )}{' '}
+      whose presence causes{' '}
+      <span className="nowrap">
+        <Text source={aura} emphasis="secondary" />.
+      </span>
     </Fragment>
   );
 }
@@ -80,7 +128,9 @@ function DemonAffinity() {
       <Text source={their} /> affinity is for{' '}
       <Text source={affinity} emphasis="primary" /> and <Text source={they} />{' '}
       <Text source={have} /> a demonic desire for{' '}
-      <Text source={desire} emphasis="secondary" />.
+      <span className="nowrap">
+        <Text source={desire} emphasis="secondary" />.
+      </span>
     </Fragment>
   );
 }
